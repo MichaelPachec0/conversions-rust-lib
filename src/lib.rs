88ca1339222bd09ee794ef554632383b82e8ec89
Michael Pacheco <git@michaelpacheco.org>
2023-01-09 23:43:42 -0800

    conversions-lib: rustfmt reformat and cleanup.
    * Implemented Associative type instead of specifying the intermediary type, makes it more generic enough and allows cleaner code (compared to restricting to types that implements certain traits).
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2023  Michael Pacheco
+ *    Copyright (c) 2023.  Michael Pacheco
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -12,30 +12,37 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+//! Library to method chain to other types.
+//! # The goal of the library
+//! This library abstracts a commonly some commonly functions in a format that allows for method
+//! chaining.
 
-use std::time::Duration;
+use core::time::Duration;
 
 /// Trait for converting a value to a `Duration`.
-pub trait ToDuration<T>
-where
-    u64: TryFrom<T>,
-{
-    /// Converts `self` to a `Duration`.
+pub trait ToDuration<T> {
+    ///The type used for intermediary conversion (in this case, `Duration` expects an u64)
+    type Intermediary;
+    /// Converts `self` to a `Duration` consuming it.
     ///
     /// # Returns
     /// A `Result` containing a new `Duration` instance if `T` was converted.
     ///
     /// # Errors
     /// If `T` overflows, return `TryFrom<T>::Error`.
-    fn to_duration(self) -> Result<Duration, <u64 as TryFrom<T>>::Error>;
+    fn to_duration(self) -> Result<Duration, <Self::Intermediary as TryFrom<T>>::Error>
+    where
+        Self::Intermediary: TryFrom<T>;
 }
 
-impl<T> ToDuration<T> for T
-where
-    u64: TryFrom<T>,
-{
-    fn to_duration(self) -> Result<Duration, <u64 as TryFrom<T>>::Error> {
-        Ok(Duration::from_secs(u64::try_from(self)?))
+impl<T> ToDuration<T> for T {
+    type Intermediary = u64;
+    #[inline]
+    fn to_duration(self) -> Result<Duration, <Self::Intermediary as TryFrom<T>>::Error>
+    where
+        Self::Intermediary: TryFrom<T>,
+    {
+        Ok(Duration::from_secs(Self::Intermediary::try_from(self)?))
     }
 }
 
@@ -45,13 +52,15 @@ mod tests {
 
     #[test]
     fn convert_duration() -> Result<(), Box<dyn std::error::Error>> {
-        let initial = 5;
+        let initial: i64 = 5;
         let solution = initial.to_duration()?;
-        let expected = Duration::from_secs(5);
-        assert_eq!(
-            solution, expected,
-            "SOLUTION {solution:?} DOES NOT EQUAL EXPECTED {expected:?}"
-        );
-        Ok(())
+        let expected = Duration::from_secs(initial as u64);
+        if solution == expected {
+            Ok(())
+        } else {
+            Err(Box::from(liberr::Err::new(format!(
+                "SOLUTION {solution:?} DOES NOT EQUAL EXPECTED {expected:?}"
+            ))))
+        }
     }
 }
